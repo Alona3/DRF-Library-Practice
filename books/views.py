9584a163bf5bf3143aa5843d8161258c3f89a918
Alona Tsapenko <alena.tsapenko7@gmail.com>
2025-07-14 14:23:25 +0200

    Add Book model, serializer, viewset and urls

--- books/views.py
+++ books/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.permissions import SAFE_METHODS, BasePermission
 
-# Create your views here.
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [permissions.IsAdminUser()]
+        return [permissions.AllowAny()]
--- library/settings.py
+++ library/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'books',
     'users',
+    'library_books',
 ]
 
 MIDDLEWARE = [
